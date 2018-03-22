d500dc5b00ca96f39b44b4d468dec6e24c1315e4
Jonathan Courat <jonathan.courat@campus-numerique-in-the-alps.com>
2018-03-22 11:56:37 +0100

    first commit

---
+++ src/main/java/game/Game.java
@@ -0,0 +1,13 @@
+package game;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Game {
+
+    public static void main(String[] args) {
+        System.out.println("Start the game");
+    }
+
+    //-- Functions --\\
+}
